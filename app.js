49ee43902ff8925cd8978c4044e8b82e23670d27
AmitBouton98 <118891774+AmitBouton98@users.noreply.github.com>
2024-02-27 11:36:17 +0200

    .

--- app.js
+++ app.js
@@ -9,53 +9,16 @@ const configPath = "./config.json";
 const config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
 
 // Define a route to fetch data from the database with parameters
-app.get("/getPDFSuggestions/:accountID/:SiteId/:AlertId", async (req, res) => {
+app.get("/getPDFSuggestions/:AccountID/:SiteId/:AlertId", async (req, res) => {
   // Alert is 1360 (code: 0x0500)
   try {
     // Extract parameters from the request
-    const { accountID, SiteId, AlertId } = req.params;
+    const { AccountID, SiteId, AlertId } = req.params;
 
     // Connect to the database
     await sql.connect(config);
 
     // Update the SQL query to use parameters
-    const result = await sql.query`
-      SELECT
-        IST.FullName AS ShirtSize,
-        COUNT(AL.CreatedDate) AS ConsumptionCount,
-        DATEPART(HOUR, AL.CreatedDate) AS HourOfDay
-      FROM
-        dbo.ItemSubTypes IST
-      JOIN
-        dbo.AlertsLog AL ON IST.Id = AL.ItemSubTypeId
-      WHERE
-        AL.AccountId = ${accountID} AND AL.SiteId = ${SiteId} AND AL.AlertId = ${AlertId}
-      GROUP BY
-        IST.FullName,
-        DATEPART(HOUR, AL.CreatedDate)
-      ORDER BY
-        IST.FullName,
-        HourOfDay;
-    `;
-
-    res.json(result.recordset);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  } finally {
-    // Close the database connection
-    await sql.close();
-  }
-});
-
-// API endpoint to fetch data
-app.get("/getAllOutOfStuckStations", async (req, res) => {
-  try {
-    await sql.connect(config);
-    const outOfStuckAlert = 1360;
-    const hospitalId = 322;
-    const countryId = 1;
-
     const result = await sql.query`
       SELECT
           AL.SiteId AS Hospital_Id,
@@ -72,10 +35,10 @@ app.get("/getAllOutOfStuckStations", async (req, res) => {
       JOIN
           ItemTypes IT ON IT.Id = IST.ItemTypeId
       WHERE
-          AL.AlertId = ${outOfStuckAlert} 
+          AL.AlertId = ${AlertId} 
           AND AL.ItemSubTypeId IS NOT NULL 
-          AND AL.AccountId = ${countryId}
-          AND AL.SiteId = ${hospitalId}
+          AND AL.AccountId = ${AccountID}
+          AND AL.SiteId = ${SiteId}
       GROUP BY
           AL.SiteId,
           ST.Name,
@@ -83,13 +46,13 @@ app.get("/getAllOutOfStuckStations", async (req, res) => {
           IST.FullName
     `;
 
-    const data = result.recordset;
 
-    res.json(data);
+    res.json(result.recordset);
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
   } finally {
+    // Close the database connection
     await sql.close();
   }
 });
