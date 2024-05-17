954c6fd7a15bf1b3b7c18b59da801fbe87bd09f5
CSgroup16 <131266999+RonAzar@users.noreply.github.com>
2024-05-17 11:00:50 +0300

    BestCommit

--- PdfCreation.js
+++ PdfCreation.js
@@ -1,6 +1,8 @@
+// Required imports
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 const { ChartJSNodeCanvas } = require("chartjs-node-canvas");
+const { generateSuggestions } = require("./Suggestions"); // Import the suggestions module
 
 // Function to prepare Chart.js data
 function prepareChartData(data) {
@@ -30,11 +32,15 @@ function prepareChartData(data) {
     const shirtData = groupedData[shirtName];
 
     // const sizes = Array.from(new Set(shirtData.map((item) => item.Item_Size)));
-    const sizes = Array.from(new Set(data.map(item => item.Item_Size.toUpperCase())));
+    const sizes = Array.from(
+      new Set(data.map((item) => item.Item_Size.toUpperCase()))
+    );
 
     sizes.forEach((size, sizeIndex) => {
       // Filter data for the current size
-      const sizeData = shirtData.filter((item) => item.Item_Size.toUpperCase() === size);
+      const sizeData = shirtData.filter(
+        (item) => item.Item_Size.toUpperCase() === size
+      );
       // Map the appearances for the current size
       const appearances = sizeData.map((item) => item.Appearance_Count);
 
@@ -68,7 +74,6 @@ function getRandomColor(alpha = 1) {
   return color;
 }
 
-
 // Function to create a Chart.js graph
 function createGraph(data) {
   const canvasRenderService = new ChartJSNodeCanvas({
@@ -92,7 +97,7 @@ function createGraph(data) {
         },
         title: {
           display: true,
-          text: "", //Distribution Of Items & Size Across All Stations
+          text: "", // Distribution Of Items & Size Across All Stations
         },
       },
       scales: {
@@ -111,7 +116,7 @@ function createGraph(data) {
   console.log("Chart created and saved as chart.png");
 }
 
-async function exportGraphAndTableToPDF() {
+async function exportGraphAndTableToPDF(tableData, mostFrequentItem) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
@@ -121,79 +126,92 @@ async function exportGraphAndTableToPDF() {
   const imageTable = fs.readFileSync("table.png", "base64");
   const imageTableSrc = `data:image/png;base64,${imageTable}`;
 
+  // Generate suggestions
+  const suggestions = generateSuggestions(tableData, mostFrequentItem);
+
+  // Create suggestions HTML
+  const suggestionsHtml = `
+  <h2>Analytics Results & Suggestions</h2>
+  <ol>
+    ${suggestions.map((suggestion) => `<li>${suggestion}</li>`).join("")}
+  </ol>
+`;
+
   // Set the HTML content for the PDF
   const htmlContent = `
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Out Of Stock Analysis</title>
-        <style>
-          body {
-            font-family: 'Arial', sans-serif;
-            margin: 20px;
-          }
-  
-          h1 {
-            color: #333;
-          }
-  
-          span {
-            display: block;
-            margin-bottom: 20px;
-            color: #555;
-          }
-  
-          h2 {
-            color: #333;
-          }
-  
-          img {
-            max-width: 100%;
-            height: auto;
-          }
-  
-          table {
-            border-collapse: collapse;
-            width: 100%;
-          }
-  
-          th, td {
-            padding: 8px;
-            text-align: center;
-          }
-  
-          th {
-            background-color: #f2f2f2;
-          }
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Out Of Stock Analysis</title>
+    <style>
+      body {
+        font-family: 'Arial', sans-serif;
+        margin: 20px;
+      }
 
-   
-          
-        </style>
-      </head>
-      <body>
-      <div>
+      h1 {
+        color: #333;
+      }
+
+      span {
+        display: block;
+        margin-bottom: 20px;
+        color: #555;
+      }
+
+      h2 {
+        color: #333;
+      }
+
+      img {
+        max-width: 100%;
+        height: auto;
+      }
+
+      table {
+        border-collapse: collapse;
+        width: 100%;
+      }
+
+      th, td {
+        padding: 8px;
+        text-align: center;
+      }
+
+      th {
+        background-color: #f2f2f2;
+      }
+    </style>
+  </head>
+  <body>
+    <div>
       <h1>Out Of Stock Analysis - Hospital Name - Date</h1>
-      </div>  
-        <div>
-        <h3>General</h3>
-        <span>
-          As the vendor for the automatic garment dispensing units at Hospital Name, Polytex has unique insight into the inventory management practices and supply chain operations of the hospital. <br>
-          In this <strong>"out of stock"</strong> analysis, Polytex will leverage this expertise to provide a comprehensive overview of the factors that contributed to the shortages experienced by the hospital in Date. This analysis will draw on data collected from the automatic garments dispensing units to identify areas of inefficiency or mismanagement that may have contributed to the shortages. Additionally, Polytex will provide recommendations for how the hospital can improve its inventory management practices and supply chain operations to ensure that critical equipment and supplies are always available when they are needed. <br>
-          Through this report, Polytex hopes to help Hospital Name optimize their operations and improve patient outcomes.
-        </span>
-  
-        </div>
-        <div> 
-        <h3>Distribution Of Items & Size Across All Stations</h3>
-        <div style="height: 25%;"><img src="${imageSrc}" alt="Graph Image" /><div>
-        </div>
-          <div>
-          <div style="height: 25%;"><img src="${imageTableSrc}"alt="Table Image" /></div>
-          </div>
-      </body>
-      </html>
-      `;
+    </div>  
+    <div>
+      <h3>General</h3>
+      <span>
+        As the vendor for the automatic garment dispensing units at Hospital Name, Polytex has unique insight into the inventory management practices and supply chain operations of the hospital. <br>
+        In this <strong>"out of stock"</strong> analysis, Polytex will leverage this expertise to provide a comprehensive overview of the factors that contributed to the shortages experienced by the hospital in Date. This analysis will draw on data collected from the automatic garments dispensing units to identify areas of inefficiency or mismanagement that may have contributed to the shortages. Additionally, Polytex will provide recommendations for how the hospital can improve its inventory management practices and supply chain operations to ensure that critical equipment and supplies are always available when they are needed. <br>
+        Through this report, Polytex hopes to help Hospital Name optimize their operations and improve patient outcomes.
+      </span>
+    </div>
+    <div> 
+      <h3>Distribution Of Items & Size Across All Stations</h3>
+      <div style="height: 25%;"><img src="${imageSrc}" alt="Graph Image" /><div>
+    </div>
+    <div>
+      <div style="height: 25%;"><img src="${imageTableSrc}" alt="Table Image" /></div>
+    </div>
+    ${suggestionsHtml}
+    <div>
+    This report by Polytex provides <strong>data-driven insights</strong> into inventory management and supply chain optimization at <strong>Hospital name</strong>.
+    Based on the data, the hospital can consider adjusting inventory levels, optimizing supply chain operations, improving forecasting accuracy, and implementing automated inventory management solutions.
+    Follow-through on these recommendations can help improve personal outcomes and operational efficiency.
+    </div>
+  </body>
+  </html>
+`;
 
   // Set the HTML content of the page
   await page.setContent(htmlContent, { waitUntil: "domcontentloaded" });
@@ -210,38 +228,38 @@ async function exportGraphAndTableToPDF() {
 // Function to generate table HTML content with dynamic size columns
 async function generateTableHTML(tableData) {
   let tableHtml = `
-          <h2 style="text-align: center;">Distribution Of Sizes Per Dispense Unit</h2>
-          <table border="1" style="width: 100%; border-collapse: collapse;">
-            <thead style="background-color: #f2f2f2;">
-              <tr>
-                <th style="padding: 8px; text-align: left;">Department</th>
+    <h2 style="text-align: center;">Distribution Of Sizes Per Dispense Unit</h2>
+    <table border="1" style="width: 100%; border-collapse: collapse;">
+      <thead style="background-color: #f2f2f2;">
+        <tr>
+          <th style="padding: 8px; text-align: left;">Department</th>
   `;
 
   // Dynamically create size columns
-  const sizes = Object.keys(tableData[0]).filter(key => key !== 'Department'); // Exclude 'Department' key
-  sizes.forEach(size => {
+  const sizes = Object.keys(tableData[0]).filter((key) => key !== "Department"); // Exclude 'Department' key
+  sizes.forEach((size) => {
     tableHtml += `<th style="padding: 8px; text-align: center;">${size}</th>`;
   });
-
 
   // Populate the table with data
   tableData.forEach((row) => {
     tableHtml += `
-              <tr>
-                <td style="padding: 8px; text-align: left;">${row.Department}</td>
+      <tr>
+        <td style="padding: 8px; text-align: left;">${row.Department}</td>
     `;
 
     // Populate size columns dynamically
-    sizes.forEach(size => {
-      tableHtml += `<td style="padding: 8px; text-align: center; background-color : ${getColor(row[size])}">${row[size]}</td>`;
+    sizes.forEach((size) => {
+      tableHtml += `<td style="padding: 8px; text-align: center; background-color: ${getColor(
+        row[size]
+      )}">${row[size]}</td>`;
     });
-
   });
 
   tableHtml += `
-            </tbody>
-          </table>
-        `;
+        </tbody>
+      </table>
+    `;
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -283,11 +301,11 @@ function getColor(value) {
 
   // Define color thresholds and corresponding colors
   const colorThresholds = [
-    { threshold: 0, color: "#FFFFCC" },    // Light yellow
+    { threshold: 0, color: "#FFFFCC" }, // Light yellow
     { threshold: 2500, color: "#E8E8E8" }, // Light gray
     { threshold: 5000, color: "#FFFF99" }, // Light yellow
     { threshold: 7500, color: "#FFCC99" }, // Light orange
-    { threshold: 10000, color: "#FFCC66" } // Light orange
+    { threshold: 10000, color: "#FFCC66" }, // Light orange
   ];
 
   // Find the two closest thresholds
@@ -295,47 +313,27 @@ function getColor(value) {
   let upperThreshold = { threshold: Infinity, color: "white" };
 
   for (let i = 0; i < colorThresholds.length; i++) {
-    if (colorThresholds[i].threshold <= value && colorThresholds[i].threshold > lowerThreshold.threshold) {
+    if (
+      colorThresholds[i].threshold <= value &&
+      colorThresholds[i].threshold > lowerThreshold.threshold
+    ) {
       lowerThreshold = colorThresholds[i];
     }
-    if (colorThresholds[i].threshold >= value && colorThresholds[i].threshold < upperThreshold.threshold) {
+    if (
+      colorThresholds[i].threshold >= value &&
+      colorThresholds[i].threshold < upperThreshold.threshold
+    ) {
       upperThreshold = colorThresholds[i];
     }
   }
 
   // Calculate the interpolation factor
-  const factor = (value - lowerThreshold.threshold) / (upperThreshold.threshold - lowerThreshold.threshold);
+  const factor =
+    (value - lowerThreshold.threshold) /
+    (upperThreshold.threshold - lowerThreshold.threshold);
 
   // Interpolate the color
   return interpolateColor(lowerThreshold.color, upperThreshold.color, factor);
-}
-
-
-
-function getMaxCount(queryData) {
-  let maxCount = 0; // Initialize maxCount variable
-  queryData.reduce((acc, item) => {
-    const department = item.Station_Name;
-    const size = item.Item_Size.toUpperCase(); // Convert size to uppercase for consistency
-
-    // Calculate maxCount
-    if (!acc[department]) {
-      acc[department] = {};
-    }
-
-    if (!acc[department][size]) {
-      acc[department][size] = 0;
-    }
-
-    acc[department][size] += item.Appearance_Count;
-
-    if (acc[department][size] > maxCount) {
-      maxCount = acc[department][size];
-    }
-
-    return acc;
-  }, {});
-  return maxCount;
 }
 
 // Function to generate table data from database data with dynamic size columns
@@ -344,7 +342,7 @@ function generateTableData(queryData) {
 
   // Extract unique sizes from the data
   const sizesSet = new Set();
-  queryData.forEach(item => {
+  queryData.forEach((item) => {
     sizesSet.add(item.Item_Size.toUpperCase()); // Convert size to uppercase for consistency
   });
   const sizes = Array.from(sizesSet);
@@ -372,7 +370,10 @@ function generateTableData(queryData) {
     const row = {
       Department: department,
       ...groupedData[department], // Spread size counts dynamically
-      Sum: Object.values(groupedData[department]).reduce((sum, value) => sum + value, 0),
+      Sum: Object.values(groupedData[department]).reduce(
+        (sum, value) => sum + value,
+        0
+      ),
     };
 
     tableData.push(row);
@@ -393,10 +394,8 @@ function generateTableData(queryData) {
   return tableData;
 }
 
-
 module.exports = {
   generateTableHTML,
-  getMaxCount,
   generateTableData,
   createGraph,
   exportGraphAndTableToPDF,
